5000009108cab7131ad59d482f760efce194141c
Isn0v <isnovikov0@gmail.com>
2024-08-01 21:56:54 +0700

    P.S added

--- test/PizzaTests.cpp
+++ test/PizzaTests.cpp
@@ -1,13 +1,18 @@
 
 #include <gtest/gtest.h>
+
 #include <Pizza/Pizza.hpp>
-#include <sstream>
 #include <fstream>
+#include <sstream>
 
-// // NOTE: Results differs from what is presented in the task description.
-// //       Because of the implementation details of the algorithm, we have various
-// //       ways of covering the city.
+// NOTE: Results differs from what is presented in the task description.
+//       Because of the implementation details of the algorithm, we have various
+//       ways of covering the city.
 
+// P.S: IntegrationTest/7 (Param = 8, 8'th input and expected files) is supposed
+// not to be passed because this is the test case fo which I couldn't correclty
+// expand my solution. Spent a lot of time for that and decided to skip it. I am
+// still working on it and if I success with it I will add it and send.
 
 class PizzaTest : public ::testing::TestWithParam<int> {};
 INSTANTIATE_TEST_SUITE_P(Pizza, PizzaTest, ::testing::Range(1, 10));
@@ -16,10 +21,10 @@ TEST_P(PizzaTest, IntegrationTest) {
   int num_test = GetParam();
   std::stringstream ss_in, ss_exp;
 
-  ss_in << CMAKE_PROJECT_SOURCE_DIR << "/test/data/Pizza/input_"
-        << num_test << ".txt";
-  ss_exp << CMAKE_PROJECT_SOURCE_DIR << "/test/data/Pizza/expected_"
-         << num_test << ".txt";
+  ss_in << CMAKE_PROJECT_SOURCE_DIR << "/test/data/Pizza/input_" << num_test
+        << ".txt";
+  ss_exp << CMAKE_PROJECT_SOURCE_DIR << "/test/data/Pizza/expected_" << num_test
+         << ".txt";
 
   std::ifstream in(ss_in.str());
   std::ifstream exp(ss_exp.str());
